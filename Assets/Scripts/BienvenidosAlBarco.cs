f89ebaffb00f7e2ad6b4d18eecaca1c7143650f6
Chispas25 <kanet11beach2003@gmail.com>
2025-05-08 21:46:29 +0200

    Commit del Capitán Kharell

---
+++ Assets/Scripts/BienvenidosAlBarco.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BienvenidosAlBarco : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Bienvenidos compañer@s al repositotio oficial de la demo de Lightlife, preparad las nalgas. Atentamente, Capitán Kharell");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
